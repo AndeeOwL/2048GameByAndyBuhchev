05dbb7466227f6e801ceeaa4212e11987bc8fa6b
andrean.buhchev <andeeowl@yahoo.com>
2022-10-13 12:14:09 +0300

    commented animations

--- components/PlayGrid/NumberField.js
+++ components/PlayGrid/NumberField.js
@@ -28,195 +28,195 @@ function NumberField(props) {
   let customTextStyle = styleService(props.value)[1];
 
   //SHARED VALUES
-  const offsetX = useSharedValue(0);
-  const offsetY = useSharedValue(0);
-  const opacity = useSharedValue(1);
-  const scale = useSharedValue(0);
+  // const offsetX = useSharedValue(0);
+  // const offsetY = useSharedValue(0);
+  // const opacity = useSharedValue(1);
+  // const scale = useSharedValue(0);
 
-  if (props.steps !== 0) {
-    isMoved = true;
-  }
+  // if (props.steps !== 0) {
+  //   isMoved = true;
+  // }
 
-  if (props.isNew === true || props.isMerged === true) {
-    isPoped = true;
-  }
+  // if (props.isNew === true || props.isMerged === true) {
+  //   isPoped = true;
+  // }
 
   // ANIMATIONS WITH SHARED VALUE
 
-  const animatedStyles = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateX: offsetX.value }, { translateY: offsetY.value }],
-      opacity: opacity.value,
-    };
-  }, []);
+  //   const animatedStyles = useAnimatedStyle(() => {
+  //     return {
+  //       transform: [{ translateX: offsetX.value }, { translateY: offsetY.value }],
+  //       opacity: opacity.value,
+  //     };
+  //   }, []);
 
-  const animatedTextStyles = useAnimatedStyle(() => {
-    return {
-      transform: [{ scale: scale.value }],
-    };
-  }, []);
+  //   const animatedTextStyles = useAnimatedStyle(() => {
+  //     return {
+  //       transform: [{ scale: scale.value }],
+  //     };
+  //   }, []);
 
-  useLayoutEffect(() => {
-    if (isMoved === true) {
-      const move = moveAnimationService(props.direction, props.steps);
+  //   useLayoutEffect(() => {
+  //     if (isMoved === true) {
+  //       const move = moveAnimationService(props.direction, props.steps);
 
-      if (props.direction === "LEFT" || props.direction === "RIGHT") {
-        offsetX.value = withTiming(move, { duration: 500 });
-      } else if (props.direction === "UP" || props.direction === "DOWN") {
-        offsetY.value = withTiming(move, { duration: 500 });
-      }
+  //       if (props.direction === "LEFT" || props.direction === "RIGHT") {
+  //         offsetX.value = withTiming(move, { duration: 500 });
+  //       } else if (props.direction === "UP" || props.direction === "DOWN") {
+  //         offsetY.value = withTiming(move, { duration: 500 });
+  //       }
 
-      opacity.value = withTiming(0, { duration: 200 });
-    }
-  });
+  //       opacity.value = withTiming(0, { duration: 200 });
+  //     }
+  //   });
 
-  useLayoutEffect(() => {
-    if (isPoped === true) {
-      scale.value = withSpring(1);
-    }
-  });
+  //   useLayoutEffect(() => {
+  //     if (isPoped === true) {
+  //       scale.value = withSpring(1);
+  //     }
+  //   });
+
+  //   return (
+  //     <View style={styles.border}>
+  //       <Animated.View
+  //         style={
+  //           isMoved
+  //             ? [styles.gridTextContainer, customGridStyle, animatedStyles]
+  //             : [styles.gridTextContainer, customGridStyle]
+  //         }
+  //       >
+  //         {props.value === 0 ? (
+  //           <Animated.Text
+  //             style={
+  //               props.isNew
+  //                 ? [customTextStyle, animatedTextStyles]
+  //                 : customTextStyle
+  //             }
+  //           ></Animated.Text>
+  //         ) : (
+  //           <Animated.Text
+  //             style={
+  //               props.isNew
+  //                 ? [customTextStyle, animatedTextStyles]
+  //                 : customTextStyle
+  //             }
+  //           >
+  //             {props.value}
+  //           </Animated.Text>
+  //         )}
+  //       </Animated.View>
+  //     </View>
+  //   );
+  // }
+  //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  // ANIMATIONS WITH LAYOUT ANIMATION
+
+  //   if (props.direction === "UP") {
+  //     return (
+  //       <View style={styles.border}>
+  //         <Animated.View
+  //           entering={isMoved && SlideInDown}
+  //           style={[styles.gridTextContainer, customGridStyle]}
+  //         >
+  //           {props.value === 0 ? (
+  //             <Animated.Text style={customTextStyle}></Animated.Text>
+  //           ) : (
+  //             <Animated.Text entering={isPoped && ZoomIn} style={customTextStyle}>
+  //               {props.value}
+  //             </Animated.Text>
+  //           )}
+  //         </Animated.View>
+  //       </View>
+  //     );
+  //   }
+
+  //   if (props.direction === "DOWN") {
+  //     return (
+  //       <View style={styles.border}>
+  //         <Animated.View
+  //           entering={isMoved && SlideInUp}
+  //           style={[styles.gridTextContainer, customGridStyle]}
+  //         >
+  //           {props.value === 0 ? (
+  //             <Animated.Text style={customTextStyle}></Animated.Text>
+  //           ) : (
+  //             <Animated.Text entering={isPoped && ZoomIn} style={customTextStyle}>
+  //               {props.value}
+  //             </Animated.Text>
+  //           )}
+  //         </Animated.View>
+  //       </View>
+  //     );
+  //   }
+
+  //   if (props.direction === "LEFT") {
+  //     return (
+  //       <View style={styles.border}>
+  //         <Animated.View
+  //           entering={isMoved && SlideInRight}
+  //           style={[styles.gridTextContainer, customGridStyle]}
+  //         >
+  //           {props.value === 0 ? (
+  //             <Animated.Text style={customTextStyle}></Animated.Text>
+  //           ) : (
+  //             <Animated.Text entering={isPoped && ZoomIn} style={customTextStyle}>
+  //               {props.value}
+  //             </Animated.Text>
+  //           )}
+  //         </Animated.View>
+  //       </View>
+  //     );
+  //   }
+
+  //   if (props.direction === "RIGHT") {
+  //     return (
+  //       <View style={styles.border}>
+  //         <Animated.View
+  //           entering={props.steps > 0 && SlideInLeft}
+  //           style={[styles.gridTextContainer, customGridStyle]}
+  //         >
+  //           {props.value === 0 ? (
+  //             <Animated.Text style={customTextStyle}></Animated.Text>
+  //           ) : (
+  //             <Animated.Text entering={isPoped && ZoomIn} style={customTextStyle}>
+  //               {props.value}
+  //             </Animated.Text>
+  //           )}
+  //         </Animated.View>
+  //       </View>
+  //     );
+  //   }
+  //   if (props.direction === "") {
+  //     return (
+  //       <View style={styles.border}>
+  //         <Animated.View style={[styles.gridTextContainer, customGridStyle]}>
+  //           {props.value === 0 ? (
+  //             <Animated.Text style={customTextStyle}></Animated.Text>
+  //           ) : (
+  //             <Animated.Text style={customTextStyle}>{props.value}</Animated.Text>
+  //           )}
+  //         </Animated.View>
+  //       </View>
+  //     );
+  //   }
+  // }
+  //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  //NO ANIMATIONS
 
   return (
     <View style={styles.border}>
-      <Animated.View
-        style={
-          isMoved
-            ? [styles.gridTextContainer, customGridStyle, animatedStyles]
-            : [styles.gridTextContainer, customGridStyle]
-        }
-      >
+      <View style={[styles.gridTextContainer, customGridStyle]}>
         {props.value === 0 ? (
-          <Animated.Text
-            style={
-              props.isNew
-                ? [customTextStyle, animatedTextStyles]
-                : customTextStyle
-            }
-          ></Animated.Text>
+          <Text style={customTextStyle}></Text>
         ) : (
-          <Animated.Text
-            style={
-              props.isNew
-                ? [customTextStyle, animatedTextStyles]
-                : customTextStyle
-            }
-          >
-            {props.value}
-          </Animated.Text>
+          <Text style={customTextStyle}>{props.value}</Text>
         )}
-      </Animated.View>
+      </View>
     </View>
   );
 }
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-// ANIMATIONS WITH LAYOUT ANIMATION
-
-//   if (props.direction === "UP") {
-//     return (
-//       <View style={styles.border}>
-//         <Animated.View
-//           entering={isMoved && SlideInDown}
-//           style={[styles.gridTextContainer, customGridStyle]}
-//         >
-//           {props.value === 0 ? (
-//             <Animated.Text style={customTextStyle}></Animated.Text>
-//           ) : (
-//             <Animated.Text entering={isPoped && ZoomIn} style={customTextStyle}>
-//               {props.value}
-//             </Animated.Text>
-//           )}
-//         </Animated.View>
-//       </View>
-//     );
-//   }
-
-//   if (props.direction === "DOWN") {
-//     return (
-//       <View style={styles.border}>
-//         <Animated.View
-//           entering={isMoved && SlideInUp}
-//           style={[styles.gridTextContainer, customGridStyle]}
-//         >
-//           {props.value === 0 ? (
-//             <Animated.Text style={customTextStyle}></Animated.Text>
-//           ) : (
-//             <Animated.Text entering={isPoped && ZoomIn} style={customTextStyle}>
-//               {props.value}
-//             </Animated.Text>
-//           )}
-//         </Animated.View>
-//       </View>
-//     );
-//   }
-
-//   if (props.direction === "LEFT") {
-//     return (
-//       <View style={styles.border}>
-//         <Animated.View
-//           entering={isMoved && SlideInRight}
-//           style={[styles.gridTextContainer, customGridStyle]}
-//         >
-//           {props.value === 0 ? (
-//             <Animated.Text style={customTextStyle}></Animated.Text>
-//           ) : (
-//             <Animated.Text entering={isPoped && ZoomIn} style={customTextStyle}>
-//               {props.value}
-//             </Animated.Text>
-//           )}
-//         </Animated.View>
-//       </View>
-//     );
-//   }
-
-//   if (props.direction === "RIGHT") {
-//     return (
-//       <View style={styles.border}>
-//         <Animated.View
-//           entering={props.steps > 0 && SlideInLeft}
-//           style={[styles.gridTextContainer, customGridStyle]}
-//         >
-//           {props.value === 0 ? (
-//             <Animated.Text style={customTextStyle}></Animated.Text>
-//           ) : (
-//             <Animated.Text entering={isPoped && ZoomIn} style={customTextStyle}>
-//               {props.value}
-//             </Animated.Text>
-//           )}
-//         </Animated.View>
-//       </View>
-//     );
-//   }
-//   if (props.direction === "") {
-//     return (
-//       <View style={styles.border}>
-//         <Animated.View style={[styles.gridTextContainer, customGridStyle]}>
-//           {props.value === 0 ? (
-//             <Animated.Text style={customTextStyle}></Animated.Text>
-//           ) : (
-//             <Animated.Text style={customTextStyle}>{props.value}</Animated.Text>
-//           )}
-//         </Animated.View>
-//       </View>
-//     );
-//   }
-// }
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-//NO ANIMATIONS
-
-//   return (
-//     <View style={styles.border}>
-//       <View style={[styles.gridTextContainer, customGridStyle]}>
-//         {props.value === 0 ? (
-//           <Text style={customTextStyle}></Text>
-//         ) : (
-//           <Text style={customTextStyle}>{props.value}</Text>
-//         )}
-//       </View>
-//     </View>
-//   );
-// }
 
 export default NumberField;
 
