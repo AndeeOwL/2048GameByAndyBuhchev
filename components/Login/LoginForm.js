ca24eb5f2b95816e138775f29852c466935e4b1a
andrean.buhchev <andeeowl@yahoo.com>
2022-10-21 13:42:38 +0300

    added styled components

--- components/Login/LoginForm.js
+++ components/Login/LoginForm.js
@@ -1,18 +1,15 @@
-import { StyleSheet, TextInput, View } from "react-native";
-import { Colors } from "../../constants/Colors";
-import { Fonts } from "../../constants/Fonts";
+import { View } from "react-native";
+import { StyledTextInput } from "../StyledTextInput";
 
 function LoginForm(props) {
   return (
     <View>
-      <TextInput
-        style={styles.inputField}
+      <StyledTextInput
         placeholder='username'
         onChangeText={props.usernameChange}
         keyboardType='default'
       />
-      <TextInput
-        style={styles.inputField}
+      <StyledTextInput
         placeholder='password'
         onChangeText={props.passwordChange}
         keyboardType='default'
@@ -22,14 +19,3 @@ function LoginForm(props) {
 }
 
 export default LoginForm;
-
-const styles = StyleSheet.create({
-  inputField: {
-    padding: 10,
-    margin: 15,
-    height: 50,
-    width: 300,
-    backgroundColor: Colors.color4,
-    fontSize: Fonts.medium,
-  },
-});
---
+++ components/StyledTextInput.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+const StyledTextInput = styled.TextInput`
+  padding: 10px;
+  margin: 15px;
+  height: 50px;
+  width: 300px;
+  background-color: #ded6d5;
+  font-size: 24px;
+`;
+
+export { StyledTextInput };
